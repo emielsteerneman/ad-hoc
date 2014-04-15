26e2854c595cb2b4a38b12a9c3c3f82f2d58dd01
Æde Symen Hoekstra <ashoekstra@outlook.com>
2014-04-15 11:53:17 +0200

    a

--- src/network/NetworkListener.java
+++
@@ -1,6 +0,0 @@
-package network;
-
-
-public interface NetworkListener {
-	public void onReceive(NetworkPacket packet);
-}
--- src/network/discovery/NetworkDiscoveryListener.java
+++
@@ -1,8 +0,0 @@
-package network.discovery;
-
-import java.net.InetAddress;
-
-public interface NetworkDiscoveryListener {
-	public void onDeviceDiscovery(InetAddress device, String identifier);
-	public void onDeviceTimeout(InetAddress device);
-}
